7995eb326d0ce1eb149bd0fde0e79bdc5d0b959a
Ines Daniel <i.danie8@gmail.com>
2018-06-26 19:29:13 -0500

    created json route for all products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get "/all_products_url" => "api/products#all_products_action"
 end
