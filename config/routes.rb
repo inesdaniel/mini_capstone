401f94fc48656d35e6efd1d00126d61954f95a97
Ines Daniel <i.danie8@gmail.com>
2018-06-26 19:47:24 -0500

    added namespace api to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-  get "/all_products_url" => "api/products#all_products_action"
+  namespace :api do
+    get "/all_products_url" => "products#all_products_action"
+  end
 end
