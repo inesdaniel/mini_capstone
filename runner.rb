f9ccb27de8a819038f28b5af671a06d8eb7f05f3
Ines Daniel <i.danie8@gmail.com>
2018-06-28 22:35:08 -0500

    update create action to take in user input

--- runner.rb
+++ runner.rb
@@ -1,23 +1,24 @@
 require 'unirest'
 
+# access index action
 # response = Unirest.get("http://localhost:3000/api/products")
-
 # p response.body
 
 # p "************************"
 
+# access show action 
 # response = Unirest.get ("http://localhost:3000/api/products/3")
-
 # p response.body
 
-#create
+# implement create action
+p "Enter on each line a name of item, a description, a price and an image link."
 
 response = Unirest.post("http://localhost:3000/api/products", 
   parameters: {
-    input_name: "ruler",
-    input_description: "Measures things.",
-    input_price: 1,
-    input_image_url: ""
+    input_name: gets.chomp,
+    input_description: gets.chomp,
+    input_price: gets.chomp,
+    input_image_url: gets.chomp
   }
 )
 
