97fc0a378c79d4351f3173a5c0b8f020a43aa609
Ines Daniel <i.danie8@gmail.com>
2018-06-28 22:20:33 -0500

    make create action, add new product stapler

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,10 +3,22 @@ class Api::ProductsController < ApplicationController
     @products = Product.all 
     render "index.json.jbuilder"
   end 
-  
+
   def show
     product_id = params[:id]
     @product1 = Product.find_by(id:product_id)
     render "show.json.jbuilder"
   end
+
+  def create
+    @product1 = Product.new(
+        name: params[:input_name],
+        price: params[:input_price],
+        image_url: params[:input_image_url],
+        description: params[:input_description]
+      )
+    @product1.save
+    render "show.json.jbuilder"
+  end
+
 end
--- runner.rb
+++ runner.rb
@@ -1,9 +1,30 @@
 require 'unirest'
 
-response = Unirest.get("http://localhost:3000/api/first_product_url")
+# response = Unirest.get("http://localhost:3000/api/products")
+
+# p response.body
+
+# p "************************"
+
+# response = Unirest.get ("http://localhost:3000/api/products/3")
+
+# p response.body
+
+#create
+
+response = Unirest.post("http://localhost:3000/api/products", 
+  parameters: {
+    input_name: "ruler",
+    input_description: "Measures things.",
+    input_price: 1,
+    input_image_url: ""
+  }
+)
 
 p response.body
 
-response = Unirest.get("http://localhost:3000/api/all_products_url")
 
-p response.body
+    # input_name: params[:input_name],
+    # input_description: params[:input_description],
+    # input_price: params[:input_price],
+    # input_image_url: params[:input_image_url]
