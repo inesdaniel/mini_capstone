71bc8b2d1512c0466122ca41e09110a643698308
Ines Daniel <i.danie8@gmail.com>
2018-07-16 21:35:33 -0500

    add subtotal, tax, total to create action and display prices with $ format

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,14 +1,25 @@
 class Api::OrdersController < ApplicationController
   def show
     order_id = params[:id]
-    @order = Order.find(order_id)
+    @order = Order.find_by(id: order_id)
     render "show.json.jbuilder"
   end
 
   def create
+    #find the product
+    product = Product.find_by(id: params[:product_id])
+    #price * quantity
+    calculated_subtotal = params[:quantity].to_i * product.price 
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_tax + calculated_subtotal
+
     @order = Order.new(
+      user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
     )
     @order.save
     render "show.json.jbuilder"
--- app/views/api/orders/show.json.jbuilder
+++ app/views/api/orders/show.json.jbuilder
@@ -1,6 +1,9 @@
+json.id @order.id
 json.user_id @order.user_id
 json.product_id @order.product_id
 json.quantity @order.quantity
-json.subtotal @order.subtotal
-json.tax @order.tax
-json.total @order.total 
+json.subtotal number_to_currency(@order.subtotal) # adds $ to price to make it look like an acual price
+json.tax number_to_currency(@order.tax)
+json.total number_to_currency(@order.total) 
+json.product @order.product #established with the belongs to relationship
+
