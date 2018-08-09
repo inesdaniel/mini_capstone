68568ca43bb11a29d719e894447dfc8dc37615ca
Ines Daniel <i.danie8@gmail.com>
2018-08-08 20:41:54 -0500

    install vue.js, display products

--- public/index.js
+++ public/index.js
@@ -1,26 +1,33 @@
-/* global axios */
-console.log('files linked');
+/* global Vue, VueRouter, axios */
 
-var productTemplate = document.querySelector('#product-card');
-var productContainer = document.querySelector('.row');
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    console.log('in created function');
 
-// productContainer.appendChild(productTemplate.content.cloneNode(true));
-// productContainer.appendChild(productTemplate.content.cloneNode(true));
+    axios.get('/api/products').then(function(response) {
+      console.log(response.data);
+      this.products = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
 
-axios.get("/api/products").then(function(response) {
-  console.log(response);
-  var products = response.data;
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
 
-  products.forEach(function(product) {
-    var productClone = productTemplate.content.cloneNode(true);
-
-    productClone.querySelector('.card-title').innerText = product.name;
-    productClone.querySelector('.card-text').innerText = product.description;
-    productClone.querySelector('.id').innerText = "Id:" + product.id;
-    productClone.querySelector('.price').innerText = "Price:" + product.price;
-    productClone.querySelector('.tax').innerText = "Tax:" + product.formatted.tax;
-    productClone.querySelector('.total').innerText = "Total:" + product.formatted.total;
-
-    productContainer.appendChild(productClone);
-  });
+var app = new Vue({
+  el: "#vue-app",
+  router: router
 });
