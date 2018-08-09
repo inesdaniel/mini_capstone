b3492a032ddbfda6ba3b3905243e83776f31570a
Ines Daniel <i.danie8@gmail.com>
2018-08-08 21:22:51 -0500

    add sample route, link to navbar link

--- public/index.js
+++ public/index.js
@@ -1,4 +1,15 @@
 /* global Vue, VueRouter, axios */
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "Welcome to sample page!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 
 var HomePage = {
   template: "#home-page",
@@ -21,7 +32,10 @@ var HomePage = {
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/sample", component: SamplePage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
